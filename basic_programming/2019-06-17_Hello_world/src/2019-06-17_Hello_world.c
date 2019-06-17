5ce09d3fb31038d70127b31a1d75fba838e2d582
isalexlog <is.alex.loginov@gmail.com>
2019-06-17 21:33:05 +0200

    Hello world project

---
+++ basic_programming/2019-06-17_Hello_world/src/2019-06-17_Hello_world.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : 2019-06-17_Hello_world.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello Alexander!!!");
+	puts("!!!Hello Alexander Alexander!!!");
+	return EXIT_SUCCESS;
+}
+

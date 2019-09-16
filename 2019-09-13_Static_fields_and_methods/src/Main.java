98a5ddc3bf3ec58de32d325faedf47745e0c04f4
Alex Loginov <alexander.loginov@takeaway.com>
2019-09-16 19:20:55 +0200

    HW

--- 2019-09-13_Static_fields_and_methods/src/Main.java
+++ 2019-09-13_Static_fields_and_methods/src/Main.java
@@ -4,6 +4,21 @@ import java.lang.Math;
 
 public class Main {
 
+    /*
+    In CircleUtils add validation into every method checking if radius
+    is not negative. If radius is negative, log error with with error method
+    of Logger class. and then return -1.
+
+    Create static method that
+    - receives two integer parameters a and b,
+    - returns random integer that is less then b and greater then a.
+    Fill with the method an array of 20 integers, and print array values
+    into the console into the one line.
+    Line must start with '[' and end with ']'. Elements in the line must
+    be separated with commas (',').
+
+    */
+
     public static void main(String[] args) {
         Logger log = Logger.getLogger(Main.class);
 

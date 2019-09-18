f77c812d5bc274ea818391106d8445b5d6bb00a6
Alex Loginov <alexander.loginov@takeaway.com>
2019-09-18 19:37:33 +0200

    sample comment

--- java_basic_programming/2019-09-16_Random_integers_array/src/de/telran/Main.java
+++ java_basic_programming/2019-09-16_Random_integers_array/src/de/telran/Main.java
@@ -2,6 +2,10 @@ package de.telran;
 
 public class Main {
 
+    /*
+    I'm making this comment to demonstrate how to keep
+    up-to-date branches in the fork repository
+     */
     public static void main(String[] args) {
 
     }
